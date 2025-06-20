e5afca2eadc5e4f78829d89e8626f06c2d6ed7b0
mbadiao <diaombaye832@gmail.com>
2025-06-20 11:30:59 +0000

    implement a function to extract version number of a raw hex

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,10 @@
 // Implement extract_tx_version function below
 pub fn extract_tx_version(raw_tx_hex: &str) -> Result<u32, String> {
-    todo!()
+    let decode = hex::decode(raw_tx_hex).map_err(|_| "Hex decode error".to_string())?;
+
+    if decode.len() < 4 {
+        return Err("Transaction data too short".into());
+    }
+    let version = u32::from(decode[0]);
+    Ok(version)
 }
